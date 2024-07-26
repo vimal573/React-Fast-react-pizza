b2445be58e105cd5759365feb1e4c484657990d9
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-26 21:30:01 +0530

    Fetching Orders

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,7 +5,6 @@ import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
   const navigation = useNavigation();
-
   const isLoading = navigation.state === 'loading';
 
   return (
