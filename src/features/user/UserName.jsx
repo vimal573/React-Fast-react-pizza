40b30f1f7d66401b34acd2179233dcc6226b62cc
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-28 11:03:08 +0530

    Using CSS Flexbox

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,5 @@
 function UserName() {
-  return <div className="text-sm font-semibold">Vimal</div>;
+  return <div className="hidden text-sm font-semibold md:block">Vimal</div>;
 }
 
 export default UserName;
