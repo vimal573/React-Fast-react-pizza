00b6eb78d4c59b43e82396adc309043902380446
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-29 13:04:33 +0530

    Updating Cart Quantities

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable react/prop-types */
+import { useSelector } from 'react-redux';
 import { formatCurrency } from '../../utilitis/helpers';
 import DeleteItem from './DeleteItem';
+import UpdateItemQuantity from './UpdateItemQuantity';
+import { getCurrentQuantityById } from './cartSlice';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -13,6 +18,10 @@ function CartItem({ item }) {
       <div className="flex items-center justify-between sm:gap-6">
         <p className="mr- text-sm font-bold">{formatCurrency(totalPrice)}</p>
 
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        />
         <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
